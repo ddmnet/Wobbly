108288985ff0b62631d166b0bba4b3aecad7ec07
jacks <jacks@6b6ab1aa-1830-4753-ab79-30a21ab24d7e>
2012-01-20 21:55:27 +0000

    Added initial support for touch & hold to initiate, added css scaling
    git-svn-id: svn://dev.ddmnet.com/projectrepo/wobbly/trunk@14798 6b6ab1aa-1830-4753-ab79-30a21ab24d7e

--- src/jRumble.js
+++ src/jRumble.js
@@ -1,2 +1,126 @@
-/* jRumble v1.3 - http://jackrugile.com/jrumble - MIT License */
-(function(f){f.fn.jrumble=function(g){var a=f.extend({x:2,y:2,rotation:1,speed:15,opacity:false,opacityMin:0.5},g);return this.each(function(){var b=f(this),h=a.x*2,i=a.y*2,k=a.rotation*2,g=a.speed===0?1:a.speed,m=a.opacity,n=a.opacityMin,l,j,o=function(){var e=Math.floor(Math.random()*(h+1))-h/2,a=Math.floor(Math.random()*(i+1))-i/2,c=Math.floor(Math.random()*(k+1))-k/2,d=m?Math.random()+n:1,e=e===0&&h!==0?Math.random()<0.5?1:-1:e,a=a===0&&i!==0?Math.random()<0.5?1:-1:a;b.css("display")==="inline"&&(l=true,b.css("display","inline-block"));b.css({position:"relative",left:e+"px",top:a+"px","-ms-filter":"progid:DXImageTransform.Microsoft.Alpha(Opacity="+d*100+")",filter:"alpha(opacity="+d*100+")","-moz-opacity":d,"-khtml-opacity":d,opacity:d,"-webkit-transform":"rotate("+c+"deg)","-moz-transform":"rotate("+c+"deg)","-ms-transform":"rotate("+c+"deg)","-o-transform":"rotate("+c+"deg)",transform:"rotate("+c+"deg)"})},p={left:0,top:0,"-ms-filter":"progid:DXImageTransform.Microsoft.Alpha(Opacity=100)",filter:"alpha(opacity=100)","-moz-opacity":1,"-khtml-opacity":1,opacity:1,"-webkit-transform":"rotate(0deg)","-moz-transform":"rotate(0deg)","-ms-transform":"rotate(0deg)","-o-transform":"rotate(0deg)",transform:"rotate(0deg)"};b.bind({startRumble:function(a){a.stopPropagation();clearInterval(j);j=setInterval(o,g)},stopRumble:function(a){a.stopPropagation();clearInterval(j);l&&b.css("display","inline");b.css(p)}})})}})(jQuery);
+/*
+jRumble v1.3 - http://jackrugile.com/jrumble
+by Jack Rugile - http://jackrugile.com
+
+MIT License
+-----------------------------------------------------------------------------
+Copyright (c) 2011 Jack Rugile, http://jackrugile.com
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+(function($){
+	$.fn.jrumble = function(options){
+		
+		/*========================================================*/
+		/* Options
+		/*========================================================*/
+		var defaults = {
+			x: 2,
+			y: 2,
+			rotation: 1,
+			speed: 15,
+			opacity: false,
+			opacityMin: .5
+		},
+		opt = $.extend(defaults, options);	
+				
+		return this.each(function(){
+								  
+			/*========================================================*/
+			/* Variables
+			/*========================================================*/
+			var $this = $(this),				
+				x = opt.x*2,
+				y = opt.y*2,
+				rot = opt.rotation*2,
+				speed = (opt.speed === 0) ? 1 : opt.speed,			
+				opac = opt.opacity,
+				opacm = opt.opacityMin,
+				inline,
+				interval;
+			
+			/*========================================================*/
+			/* Rumble Function
+			/*========================================================*/		
+			var rumbler = function(){				
+				var rx = Math.floor(Math.random() * (x+1)) -x/2,
+					ry = Math.floor(Math.random() * (y+1)) -y/2,
+					rrot = Math.floor(Math.random() * (rot+1)) -rot/2,
+					ropac = opac ? Math.random() + opacm : 1;
+					
+				/*========================================================*/
+				/* Ensure Movement From Original Position
+				/*========================================================*/				
+				rx = (rx === 0 && x !== 0) ? ((Math.random() < .5) ? 1 : -1) : rx;
+				ry = (ry === 0 && y !== 0) ? ((Math.random() < .5) ? 1 : -1) : ry;	
+				
+				/*========================================================*/
+				/* Check Inline
+				/*========================================================*/
+				if($this.css('display') === 'inline'){
+					inline = true;
+					$this.css('display', 'inline-block');
+				}
+				
+				/*========================================================*/
+				/* Rumble Element
+				/*========================================================*/			
+				$this.css({
+					'position':'relative',
+					'left':rx+'px',
+					'top':ry+'px',
+					'-ms-filter':'progid:DXImageTransform.Microsoft.Alpha(Opacity='+ropac*100+')',
+					'filter':'alpha(opacity='+ropac*100+')',					
+					'-moz-opacity':ropac,					
+					'-khtml-opacity':ropac,					
+					'opacity':ropac,
+					'-webkit-transform':'rotate('+rrot+'deg)', 
+					'-moz-transform':'rotate('+rrot+'deg)', 
+					'-ms-transform':'rotate('+rrot+'deg)',
+					'-o-transform':'rotate('+rrot+'deg)', 
+					'transform':'rotate('+rrot+'deg)'
+				});
+			};
+			
+			/*========================================================*/
+			/* Rumble CSS Reset
+			/*========================================================*/
+			var reset = {
+				'left':0,
+				'top':0,
+				'-ms-filter':'progid:DXImageTransform.Microsoft.Alpha(Opacity=100)',
+				'filter':'alpha(opacity=100)',					
+				'-moz-opacity':1,					
+				'-khtml-opacity':1,					
+				'opacity':1,
+				'-webkit-transform':'rotate(0deg)',
+				'-moz-transform':'rotate(0deg)',
+				'-ms-transform':'rotate(0deg)',
+				'-o-transform':'rotate(0deg)',
+				'transform':'rotate(0deg)'
+			};
+			
+			/*========================================================*/
+			/* Rumble Start/Stop Trigger
+			/*========================================================*/
+			$this.bind({
+				'startRumble': function(e){
+					e.stopPropagation();
+					clearInterval(interval);
+					interval = setInterval(rumbler, speed)
+				},
+				'stopRumble': function(e){
+					e.stopPropagation();
+					clearInterval(interval);
+					if(inline){
+						$this.css('display', 'inline');
+					}
+					$this.css(reset);
+				}
+			});		
+			
+		});// End return this.each
+	};// End $.fn.jrumble
+})(jQuery);
--- src/wobbly.js
+++ src/wobbly.js
@@ -1,3 +1,10 @@
+/*
+function lerp( pta, ptb, t) {
+    return {x:pta.x*(1-t)+ptb.x*t, y:pta.y*(1-t)+ptb.y*t};
+}
+*/
+
+
 (function($) {
 	
 	$.fn.wobbly = function() {
@@ -32,57 +39,102 @@
 
 	};
 
+	//Default options for the plugin
 	$.fn.wobbly.defaultOptions = {
-		borderColor : "red",
-		backgroundColor : "black",
-		textColor : "green",
-		callback : function() {},
-		wobbleTime : 100	//Time in milliseconds inbetween wobble starts.
+		borderColor : "black",
+		backgroundColor : "red",
+		textColor : "white",
+		size : 1,
+		callback : function() {}
 	};
 
 	/* Private Functions */
 
 	function add_anchor($element, options) {
-		var $a = $("<a>X</a>");
+		var $span = $("<span>X</span>");
+		var $a = $("<a></a>");
 		
-		//Add css the new element.
+		//Add css the new anchor element.
 		$a.css({
-			'border' : options.borderColor,
-			'backgroundColor' : options.backgroundColor,
-			'color' : options.textColor
+			'border-color' : options.borderColor,
+			'background-color' : options.backgroundColor,
+			'color' : options.textColor,
+			'height' : (options.size * 25) + 'px',
+			'-moz-border-radius' : (options.size * 12) + 'px',
+			'-webkit-border-radius' : (options.size * 12) + 'px',
+			'width' : (options.size * 25) + 'px',
+			'display' : "block"
 		});
+
+		//Add css to span element
+		$span.css({
+			'font-size' : (options.size * 14)
+		});
+
+		//Add attribute to identify this element
+		$a.addClass("isAnchor");
 
 		//Add the delete trigger to the element.
 		$a.click(function() {
 			$(this).parent().trigger("wobbly:delete");
 		});
 
+		$span.appendTo($a);
 		$a.appendTo($element);
 
 		return $element;
 	}
 
+	function add_touch_and_hold($element) {
+		 var t;
+
+		var repeat = function () {
+		    action();
+		    t = setTimeout(repeat, start);
+		    start = start / speedup;
+		}
+
+		btn.mousedown = function() {
+		    repeat();
+		}
+
+		btn.mouseup = function () {
+		    clearTimeout(t);
+		}
+	}
+
+	function remove_anchor($element) {
+		$element.children().each(function() {
+			if($(this).hasClass("isAnchor")) {
+				$(this).remove();
+			}
+		});
+		return $element;
+	}
+
 	function start_wobble($element, options) {
 		$element = add_anchor($element, options);
+		$element = add_touch_and_hold($element);
 		$element.bind("wobbly:delete", function() {
 			$(this).fadeOut("slow", function() {
-				console.log(options);
 				$(this).wobbly("stop");
 				$(this).hide();
+				options.callback();
 			});
 		});
 
 		//start the rumble
 		$element.jrumble({
-			speed: 65
+			speed: 58,
+			x : 2,
+			y : 3,
+			rotation : 1
 		});
 		$element.trigger('startRumble');
 	}
 
 	function stop_wobble($element) {
-		//Remove x from screen.
-		//unset callback?
-		//end the rumble
+		remove_anchor($element);
 		$element.trigger('stopRumble');
 	}
 

8645f7f609dcf483ae8928094d0b206a6b74c5d2
jacks <jacks@6b6ab1aa-1830-4753-ab79-30a21ab24d7e>
2012-01-23 16:22:34 +0000

    Cleaned up delete code and example
    git-svn-id: svn://dev.ddmnet.com/projectrepo/wobbly/trunk@14825 6b6ab1aa-1830-4753-ab79-30a21ab24d7e

--- src/wobbly.js
+++ src/wobbly.js
@@ -1,10 +1,23 @@
-/*
-function lerp( pta, ptb, t) {
-    return {x:pta.x*(1-t)+ptb.x*t, y:pta.y*(1-t)+ptb.y*t};
-}
+/**
+* Wobbly - A jQuery plugin for iOS style wobblin' deletes.
+*
+* You've wanted wobbling delete icons just like iOS for awhile. 
+* Wobbly makes it easy.
+*
+* $("div").wobbly();  //Start wobbly with no callbacks and using default options.
+* $("div").wobbly("start", { //Start wobbly, with the corner "X" at twice the size
+*	size : 2,				 //and a callback.
+*	callback : function() { alert('done'); }
+* });
+* $("div").wobbly("stop");  //Stop the wobble.
+*
+* The two requirements for Wobbly are the jRumble plugin, and a webkit browser.
+*
+* @author Jack Slingerland (jacks@teamddm.com)
+* @author David Elliot (davide@teamddm.com)
+* @link http://www.teamddm.com
+* @version 1.0.0
 */
-
-
 (function($) {
 	
 	$.fn.wobbly = function() {
@@ -101,6 +114,7 @@ function lerp( pta, ptb, t) {
 			$(this).fadeOut("slow", function() {
 				$(this).wobbly("stop");
 				$(this).hide();
+				$(this).remove();
 				options.callback();
 			});
 		});
@@ -119,8 +133,5 @@ function lerp( pta, ptb, t) {
 		remove_anchor($element);
 		$element.trigger('stopRumble');
 	}
-
-
-
 
 })(jQuery);
